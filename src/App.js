a1bf07a62f4f4e829e6e4f2b40763c55ffd87527
My Name <myemail@example.com>
2021-09-17 15:51:19 +0500

    pages update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import AppDevelopment from './Components/pages/AppDevelopment';
+import Home from './Components/pages/Home';
+import AboutUs from './Components/pages/AboutUs';
+import WebDevelopment from './Components/pages/WebDevelopment';
+import Contact from './Components/pages/Contact';
 
 
-
-const App = () => {
-    return (
-        <div>
-              
-        </div>
-    )
+function App() {
+return (
+	<Router>
+	<Navbar />
+	<Switch>
+		<Route path='/' exact component={Home} />
+		<Route path='/about' component={AboutUs} />
+		<Route path='/appdevelopment' component={AppDevelopment} />
+		<Route path='/webdevelopment' component={WebDevelopment} />
+		<Route path='/contactus' component={Contact} />
+		
+	</Switch>
+	</Router>
+);
 }
 
-export default App
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/' activeStyle>
+			Home
+		</NavLink>
+		<NavLink to='/aboutus' activeStyle>
+			About Us
+		</NavLink>
+		<NavLink to='/appdevelopment' activeStyle>
+			App Development
+		</NavLink>
+		<NavLink to='/webdevelopment' activeStyle>
+			Web Development
+		</NavLink>
+		<NavLink to='/contactus' activeStyle>
+			Contact US 
+		</NavLink>
+		</NavMenu>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
