2c9e2d78bac9b8a2dc0f79fc0211ae1dc70dc1dc
Wahid <wahid03129009520@gmail.com>
2021-09-26 15:35:30 +0500

    # component added # router added # other fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React from 'react'
+import { Link, Route, Switch } from 'react-router-dom'
+import Home from "./components/Home";
+import About from "./components/About";
+import WebDevelopment from './components/WebDevelopmet';
+import AppDevelopment from './components/AppDevelopment';
+import ContactUs from './components/ContactUs';
 
 
 //branche 
 const App = () => {
     return (
-        <div>
-            
+        
+    <div>
+        <div className='Nav-part'>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/web-development">Web Development</Link>
+          </li>
+          <li>
+            <Link to="/app-development">App Development</Link>
+          </li>
+          <li>
+            <Link to="/contact-us">Contact Us</Link>
+          </li>
+
+        </ul>
+
+        </div>
+      
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+           <About/>   
+          </Route>
+          <Route path="/web-development">
+              <WebDevelopment/>
+          </Route>
+          <Route path="/app-development">
+              <AppDevelopment />
+          </Route>
+          <Route path="/contact-us">
+              <ContactUs/>
+          </Route>
+        </Switch>
         </div>
     )
 }
