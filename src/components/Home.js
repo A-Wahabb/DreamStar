171fd4d0b041b7b7892b0fa3279395c29502657d
My Name <myemail@example.com>
2021-09-29 16:44:29 +0500

    slider add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './Home.css';
+import './ParticalScreen/Slider';
 
 const Home=()=>{
     return(
         <div>
-            
+            <Slider/>
         </div>
     )
 }
