dd98efe8d84c90c4c546330c91f98ef8118e632b
My Name <myemail@example.com>
2021-09-30 19:05:04 +0500

    slider add

--- src/components/ParticalScreen/Slider.js
+++ src/components/ParticalScreen/Slider.js
@@ -1,14 +1,50 @@
 import React from 'react'
 import './slider.css'
-// import { Carousel } from 'antd';
+ import { Carousel } from 'antd';
+
+ //importing slider images
+ import slideone from '../../../public/Slider/slideone.jpg'
+ import webDevelopment from '../../../public/Slider/webDevelopment.jpg'
+ import AppDevelopment from '../../../public/Slider/AppDevelopment.jpg'
 
 
 const Slider = () => {
+    const [isMobile, setIsMobile] = useState(false)
+    const handleResize = () => {
+        if (window.innerWidth < 600) {
+            setIsMobile(true)
+            console.info(isMobile)
+        } else {
+            setIsMobile(false)
+          
+        }
+      }
+      useEffect(()=>{
+        handleResize()
+       
+    },[])
+      useEffect(()=>{
+          window.addEventListener('resize',handleResize)
+      })
     return (
-        <div className='slider'>
-            
-   
+        <div>
+        <div  className='heroBlock'>
+        <Carousel autoplay effect='scrollx'>
+    <div>
+        <img src={slideone} alt='slideone'  width='100%' height={!isMobile?'600px':'350px'}/>
+    </div>
+    <div>
+        <img src={webDevelopment} alt='webDevelopment'  width='100%' height={!isMobile?'600px':'350px'}/>
+    </div>
+    <div>
+        <img src={AppDevelopment} alt='AppDevelopment' width='100%' height={!isMobile?'600px':'350px'}/>
+    </div>
+    
+    
+  </Carousel>
         </div>
+
+    </div>
     )
 }
 
