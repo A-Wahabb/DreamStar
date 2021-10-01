5ae6e2415ee8b0be2f3f67fd858a3979834498bb
Wahid <wahid03129009520@gmail.com>
2021-10-01 16:28:16 +0500

    home changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,6 @@
-import React from "react";
-import { Carousel } from 'antd';
-import dsclogo from '../dsclogo.png'
-const contentStyle = {
-    width:'100%',
-    height: '600px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
+import React from "react"
+import { Carousel } from 'antd'
+import './home.css'
 
 const Home=()=>{
     return(
@@ -17,16 +9,16 @@ const Home=()=>{
            <div className='slider'>
            <Carousel style={{width:'100%'}}  >
            <div>
-           <img alt='Not Found' style={contentStyle} src={process.env.PUBLIC_URL+'/slideone.jpg'}/>
+           <img alt='Not Found' className='contentStyle' src={process.env.PUBLIC_URL+'/slideone.jpg'}/>
           </div>    
            <div>
-           <img alt='Not Found' style={contentStyle} src={process.env.PUBLIC_URL+'/ui.jpg'}/>
+           <img alt='Not Found' className='contentStyle'src={process.env.PUBLIC_URL+'/ui.jpg'}/>
           </div>
           <div>
-           <img alt='Not Found' style={contentStyle} src={process.env.PUBLIC_URL+'/app.jpg'}/>
+           <img alt='Not Found' className='contentStyle'src={process.env.PUBLIC_URL+'/app.jpg'}/>
           </div>
           <div>
-           <img alt='Not Found' style={contentStyle} src={process.env.PUBLIC_URL+'/web.jpg'}/>
+           <img alt='Not Found' className='contentStyle'src={process.env.PUBLIC_URL+'/web.jpg'}/>
           </div>
   </Carousel>
            </div>
