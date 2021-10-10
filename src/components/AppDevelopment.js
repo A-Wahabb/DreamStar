ae32422cf1178846103a379bfbfcfdbae0cbfa2b
My Name <myemail@example.com>
2021-10-10 13:08:44 +0500

    name change

--- src/components/AppDevelopment.js
+++ src/components/AppDevelopment.js
@@ -8,25 +8,45 @@ const AppDevelopment=()=>{
          <div>
          <img alt='AppDevelopment' className='contentStyle'src={process.env.PUBLIC_URL+'/app.jpg'}/>
          </div>
-         <div className='AppDevelopment_content'>
-             <div className='AppDevelopment_heading'>
-                 <h1 style={{color:'#448ee4'}}>App Development</h1>
-                 </div>
-               <div className='AppDevelopment_peragraf' >
-                       <p>
+         <div  className='AppDevelopment_content'>
+             <div>
+                 <h1 className='AppDevelopment_heading'>App Development</h1>
+                       <p className='AppDevelopment_peragraf' >
                            dreamStarCoder provides complete software product development services, software lifecycle and
                            updates services. the company has many years experiance in design, development and maintenance
                            of software products. we take ideas from which we design and build software adding innovation and
                            creativity into the product. 
                        </p>
-                       <p>
+                       <p className='AppDevelopment_peragraf'>
                            we naver give a commitment that we can't keep. we challenge ourselves with the most 
                            current and cutting-edge problems and we take pride in solving them. we are highly qualified 
                            to provide software solution which are reliable, flexible, essy to use.
                        </p>
-               </div>
-         </div>
+              </div>
+         
+          
+          <div>
+              <h1 className='AppDevelopment_heading'>project Manager</h1>
+              <p className='AppDevelopment_peragraf'>
+                before getting in to the work. our project management team first analyses the real requirements
+                 functionality. staffing and objectives. we never make a commitment that we can't keep. we spend 
+                 good time in writing a plan. and we estimate the time lines based on the effort we have to put and 
+                 we feel the task is completed when they are 100% complete.
+              </p>
+               <p className='AppDevelopment_peragraf'>
+                   we follow a development process to make a successful completion of the assigment we undertake. 
+                   our process involves business analysis, specification, system analysis and design, implementation & 
+                   testing.
+               </p>
 
+          </div>
+          <div>
+              <h1 className='AppDevelopment_heading'>technology we use</h1>
+              <div>
+                  <img alt='Andriod'  src={process.env.PUBLIC_URL+'/andriondicon.png'} width='100px' height='100px'/>
+              </div>
+          </div>
+         </div>
         </div>
     )
 }
