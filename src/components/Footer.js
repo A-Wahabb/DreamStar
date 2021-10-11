4777649199ae40533556ad8d43af1ff90f06d680
My Name <myemail@example.com>
2021-10-11 14:46:05 +0500

    sage icon added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,10 +18,18 @@ const Footer = () => {
                                Follow Us
                                </h2>
                                <div className='socialicon'>
-                             <div className='socialicondiv'>  <img alt='Facebook' src={process.env.PUBLIC_URL+'\\facebok.png'} title='facebook' className='socialicon'  height={48} width={48} /></div>
-                              <div className='socialicondiv'> <img alt='Twitwer' src={process.env.PUBLIC_URL+'\\twitw.png'} title='twitwer' className='socialicon'  height={48} width={48} /></div>
-                               <div className='socialicondiv'><img alt='Linkdin' src={process.env.PUBLIC_URL+'\\linkdin.png'} title='linkdin' className='socialicon' height={48} width={48} /></div>
-                               <div className='socialicondiv'><img alt='Whatsapp' src={process.env.PUBLIC_URL+'\\watsapp.png'} title='wathsapp' className='socialicon' height={48} width={48} /></div>
+                             <div className='socialicondiv'>  
+                             <img alt='Facebook' src={process.env.PUBLIC_URL+'\\facebok.png'} title='facebook' className='socialicon' />
+                             </div>
+                              <div className='socialicondiv'>
+                                   <img alt='Twitwer' src={process.env.PUBLIC_URL+'\\twitw.png'} title='twitwer' className='socialicon'  />
+                                   </div>
+                               <div className='socialicondiv'>
+                                   <img alt='Linkdin' src={process.env.PUBLIC_URL+'\\linkdin.png'} title='linkdin' className='socialicon'  />
+                                   </div>
+                               <div className='socialicondiv'>
+                                   <img alt='Whatsapp' src={process.env.PUBLIC_URL+'\\watsapp.png'} title='wathsapp' className='socialicon' />
+                                   </div>
                                   </div>       
                             </div>
                             <div  className='contact_us'>
@@ -29,23 +37,22 @@ const Footer = () => {
                                Contact Information 
                                     </h2>
                                     <div className='logo'>
-         <img src={process.env.PUBLIC_URL+'\\dsclogo.png'} title='DreamStarCoder'  height={110} width={100} />
+         <img src={process.env.PUBLIC_URL+'\\dsclogo.png'} title='DreamStarCoder'  height={90} width={74} />
          </div>
          <div>
-            <p> DreamStarCoder (pvt) limited</p>
-            <div>
-                <img alt='g-mail' src={process.env.PUBLIC_URL+'/'} />
+            <p className='company_name'> DreamStarCoder (pvt) limited</p>
+            <div className='gmail_div'>
+                <img alt='g-mail' className='gmail_icon' src={process.env.PUBLIC_URL+'/email.png'} />
+                 <p className='Eamil_address'>dream.starcoder@gmail.com</p>
             </div>
-            <p>dream.starcoder@gmail.com</p>
+           
             <p>+923130908533</p>
          </div>
            </div>
         </div>
-         <div className='copyright_note'>
                <div> 
-                   <h3 style={{color:'#fff'}}>Copyright @ 2021 DreamStarCoder (pvt) limited</h3>
+                   <h3 className='copyright_note'>Copyright @ 2021 DreamStarCoder (pvt) limited</h3>
                    </div>
-        </div>
         </div>
     )
 }
